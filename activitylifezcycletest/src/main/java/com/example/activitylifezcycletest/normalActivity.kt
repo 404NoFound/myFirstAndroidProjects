0f45ab6aad2933bc3429d43347eef2731a682f9f
面包我请 <xsxiaosa@gmail.com>
2017-02-22 01:51:10 +0800

    init

---
+++ activitylifezcycletest/src/main/java/com/example/activitylifezcycletest/normalActivity.kt
@@ -0,0 +1,12 @@
+package com.example.activitylifezcycletest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class normalActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_normal)
+    }
+}
